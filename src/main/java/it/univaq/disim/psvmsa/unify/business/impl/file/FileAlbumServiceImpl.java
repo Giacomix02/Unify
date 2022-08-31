483ad778a6075cdb8ac5ade486d7badac5637bd2
Specy <specy.dev@gmail.com>
2022-08-31 09:53:04 +0200

    created template for Album and Artist file service

---
+++ src/main/java/it/univaq/disim/psvmsa/unify/business/impl/file/FileAlbumServiceImpl.java
@@ -0,0 +1,32 @@
+package it.univaq.disim.psvmsa.unify.business.impl.file;
+import it.univaq.disim.psvmsa.unify.business.AlbumService;
+import it.univaq.disim.psvmsa.unify.business.BusinessException;
+import it.univaq.disim.psvmsa.unify.model.Album;
+
+public class FileAlbumServiceImpl implements AlbumService {
+
+    @Override
+    public Album getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) throws BusinessException {
+
+    }
+
+    @Override
+    public int add(Album album) {
+        return 0;
+    }
+
+    @Override
+    public void update(Album album) throws BusinessException {
+
+    }
+
+    @Override
+    public void delete(Album album) throws BusinessException {
+
+    }
+}
---
+++ src/main/java/it/univaq/disim/psvmsa/unify/business/impl/file/FileArtistServiceImpl.java
@@ -0,0 +1,33 @@
+package it.univaq.disim.psvmsa.unify.business.impl.file;
+
+import it.univaq.disim.psvmsa.unify.business.ArtistService;
+import it.univaq.disim.psvmsa.unify.business.BusinessException;
+import it.univaq.disim.psvmsa.unify.model.Artist;
+
+public class FileArtistServiceImpl implements ArtistService {
+
+    @Override
+    public Artist getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) throws BusinessException {
+
+    }
+
+    @Override
+    public int add(Artist artist) {
+        return 0;
+    }
+
+    @Override
+    public void update(Artist artist) throws BusinessException {
+
+    }
+
+    @Override
+    public void delete(Artist artist) throws BusinessException {
+
+    }
+}
