8f76dc2e1ec0a93ec0bd7e33572fe7add499dad1
Giacomix02 <paoloccigiacomo@gmail.com>
2022-09-01 10:54:37 +0200

    finished FileSongServiceImpl

--- src/main/java/it/univaq/disim/psvmsa/unify/business/impl/file/FileSongServiceImpl.java
+++ src/main/java/it/univaq/disim/psvmsa/unify/business/impl/file/FileSongServiceImpl.java
@@ -1,7 +1,13 @@
 package it.univaq.disim.psvmsa.unify.business.impl.file;
 
 import it.univaq.disim.psvmsa.unify.business.*;
+import it.univaq.disim.psvmsa.unify.model.Genre;
 import it.univaq.disim.psvmsa.unify.model.Song;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.IdentityHashMap;
+import java.util.List;
 
 public class FileSongServiceImpl implements SongService {
     private static class Schema{
@@ -21,6 +27,7 @@ public class FileSongServiceImpl implements SongService {
     private ArtistService artistService;
     private AlbumService albumService;
     private PictureService pictureService;
+    private GenreService genreService;
     private final String SEPARATOR = "|";
     private final String songsFolderPath;
     public FileSongServiceImpl(
@@ -41,13 +48,32 @@ public class FileSongServiceImpl implements SongService {
 
     public Song getById(Integer id) {
         IndexedFile file = loader.load();
+        IndexedFile relationFile = genresRelationLoader.load();
+
         IndexedFile.Row row = file.findRowById(id);
 
+        List<IndexedFile.Row> relationRows = relationFile.filterRows(
+                r -> r.getIntAt(RelationSchema.SONG_ID)==id
+        );
+
         Song song = new Song(row.getIntAt(Schema.SONG_ID));
+
         song.setName(row.getStringAt(Schema.SONG_NAME));
         song.setLyrics(row.getStringAt(Schema.LYRICS));
+        song.setAlbum(albumService.getById(row.getIntAt(Schema.ALBUM_ID)));
+        song.setArtist(artistService.getById(row.getIntAt(Schema.ARTIST_ID)));
+        song.setPicture(pictureService.getById(row.getIntAt(Schema.PICTURE_ID)));
 
-        //TODO finish getById of Song file impl
+        List<Genre> genres = new ArrayList<>();
+
+        for(IndexedFile.Row relationRow:relationRows){
+            Genre genre = genreService.getById(relationRow.getIntAt(RelationSchema.GENRE_ID));
+            genres.add(genre);
+        }
+
+        song.setGenres(genres);
+
+
 
         return song;
     }
@@ -59,7 +85,22 @@ public class FileSongServiceImpl implements SongService {
     }
 
     public void update(Song song) throws BusinessException {
+        IndexedFile file = loader.load();
+        IndexedFile relationFile = genresRelationLoader.load();
 
+        IndexedFile.Row row = new IndexedFile.Row(SEPARATOR);
+
+        row.set(Schema.SONG_ID,song.getId())
+            .set(Schema.SONG_NAME,song.getName())
+            .set(Schema.LYRICS,song.getLyrics())
+            .set(Schema.ALBUM_ID,song.getAlbum().getId())
+            .set(Schema.ARTIST_ID,song.getArtist().getId())
+            .set(Schema.PICTURE_ID,song.getPicture().getId());
+
+
+
+        file.updateRow(row);
+        loader.save(file);
 
     }
 
@@ -73,12 +114,12 @@ public class FileSongServiceImpl implements SongService {
         IndexedFile file = loader.load();
         IndexedFile.Row row = new IndexedFile.Row(SEPARATOR);
 
-        row.set(Schema.LYRICS, song.getLyrics());
-        row.set(Schema.SONG_NAME, song.getName());
-        row.set(Schema.ARTIST_ID, song.getArtist().getId());
-        row.set(Schema.ALBUM_ID, song.getAlbum().getId());
-        row.set(Schema.PICTURE_ID, song.getPicture().getId());
-        row.set(Schema.SONG_ID, song.getId());
+        row.set(Schema.SONG_NAME, song.getName())
+            .set(Schema.ARTIST_ID, song.getArtist().getId())
+            .set(Schema.ALBUM_ID, song.getAlbum().getId())
+            .set(Schema.PICTURE_ID, song.getPicture().getId())
+            .set(Schema.SONG_ID, song.getId())
+            .set(Schema.LYRICS, song.getLyrics());
 
         file.appendRow(row);
         loader.save(file);
