6c995919f6cbf8c3e2f954891673b7f35035e3ec
Giacomix02 <paoloccigiacomo@gmail.com>
2022-09-10 20:52:56 +0200

    fixed graphic bug visualization in addGenre.fxml

--- src/main/java/it/univaq/disim/psvmsa/unify/controller/AddGenreController.java
+++ src/main/java/it/univaq/disim/psvmsa/unify/controller/AddGenreController.java
@@ -33,6 +33,7 @@ public class AddGenreController implements Initializable, DataInitializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         this.saveButton
                 .disableProperty()
                 .bind(genreInput
