acb89c9727c5163b9916f0e9caa8a69e1ce5e152
Specy <specy.dev@gmail.com>
2022-12-01 14:50:57 +0100

    Forgot AddLinkButton

---
+++ src/main/java/it/univaq/disim/psvmsa/unify/view/components/AddLinkButton.java
@@ -0,0 +1,30 @@
+package it.univaq.disim.psvmsa.unify.view.components;
+
+import it.univaq.disim.psvmsa.unify.view.Pages;
+import it.univaq.disim.psvmsa.unify.view.ViewDispatcher;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+public class AddLinkButton extends HBox {
+    public AddLinkButton(Pages page) {
+        super();
+        try {
+            HBox root = FXMLLoader.load(getClass().getResource("/ui/components/Add.fxml"));
+            HBox.setHgrow(root, Priority.ALWAYS);
+            getChildren().add(root);
+            Button addButton = (Button) root.lookup("#button");
+            addButton.setOnAction(event -> {
+                try{
+                    ViewDispatcher.getInstance().navigateTo(page);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
