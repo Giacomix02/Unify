deb51fc200f1f2922cf467f7b7f7ff9bbc8a1c1a
Giacomix02 <paoloccigiacomo@gmail.com>
2022-12-16 19:51:24 +0100

    one controller to edit and create a playlist

---
+++ src/main/java/it/univaq/disim/psvmsa/unify/controller/EditPlaylistController.java
@@ -0,0 +1,97 @@
+package it.univaq.disim.psvmsa.unify.controller;
+
+
+import it.univaq.disim.psvmsa.unify.business.PlaylistService;
+import it.univaq.disim.psvmsa.unify.business.UnifyServiceFactory;
+import it.univaq.disim.psvmsa.unify.model.Playlist;
+import it.univaq.disim.psvmsa.unify.model.User;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+
+public class EditPlaylistController implements Initializable, DataInitializable{
+
+    @FXML
+    private TextField playlistInput;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button deleteButton;
+
+    private Playlist playlist;
+    private User user;
+    @FXML
+    private Label exceptionLabel;
+
+    private PlaylistService playlistService;
+
+    public EditPlaylistController(){
+        UnifyServiceFactory factoryInstance = UnifyServiceFactory.getInstance();
+        this.playlistService = factoryInstance.getPlaylistService();
+    }
+
+    public void initializeData(Object data) {
+
+        if(data instanceof Playlist){
+            this.playlist = (Playlist) data;
+            this.playlistInput.setText(playlist.getName());
+            this.deleteButton.setVisible(true);
+            this.saveButton.setText("Edit");
+        }else if(data instanceof User){
+            this.user = (User) data;
+            this.deleteButton.setVisible(false);
+            this.saveButton.setText("Save");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        exceptionLabel.setText("");
+
+        this.saveButton
+                .disableProperty()
+                .bind(playlistInput
+                        .textProperty()
+                        .isEmpty()
+                );
+    }
+
+    public void savePlaylist(){
+        if (user != null) {
+            Playlist playlist = new Playlist(playlistInput.getText(),user);
+            playlistService.add(playlist);
+            playlistInput.clear();
+            exceptionLabel.setText("Playlist saved");
+        }else{
+            playlist.setName(playlistInput.getText());
+            try {
+            playlistService.update(playlist);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            exceptionLabel.setText("Playlist edited");
+        }
+    }
+
+    public void deletePlaylist(){
+        try{
+            playlistService.delete(playlist);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        exceptionLabel.setText("Playlist deleted");
+    }
+
+
+    public void textFieldClick(){
+        exceptionLabel.setText("");
+    }
+}
--- src/main/java/it/univaq/disim/psvmsa/unify/view/components/ViewPlaylist.java
+++ src/main/java/it/univaq/disim/psvmsa/unify/view/components/ViewPlaylist.java
@@ -1,14 +1,20 @@
 package it.univaq.disim.psvmsa.unify.view.components;
 
 import it.univaq.disim.psvmsa.unify.model.Playlist;
+import it.univaq.disim.psvmsa.unify.model.User;
+import it.univaq.disim.psvmsa.unify.view.Pages;
+import it.univaq.disim.psvmsa.unify.view.ViewDispatcher;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 public class ViewPlaylist extends HBox {
 
+    private User user;
     private Label label;
+    private Button editButton;
 
     private Playlist playlist;
 
@@ -25,6 +31,16 @@ public class ViewPlaylist extends HBox {
             getChildren().add(root);
 
             label = (Label) root.lookup("#label");
+            editButton = (Button) root.lookup("#editButton");
+
+
+            this.editButton.setOnMouseClicked(event -> {
+                try {
+                    ViewDispatcher.getInstance().navigateTo(Pages.EDITPLAYLIST,playlist);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
 
             label.setText(playlist.getName());
 
